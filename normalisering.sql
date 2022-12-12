6a37579b4d4b9ad50e17b845f1265d699c4b35af
samuel-satter <samuel.satter@hotmail.com>
2022-12-12 09:38:23 +0100

    readded sql

--- normalisering.sql
+++ normalisering.sql
@@ -1,11 +1,39 @@
-use iths;
+
+USE iths;
+
+DROP TABLE IF EXISTS UNF;
+
+CREATE TABLE `UNF` (
+    `Id` DECIMAL(38, 0) NOT NULL,
+    `Name` VARCHAR(26) NOT NULL,
+    `Grade` VARCHAR(11) NOT NULL,
+    `Hobbies` VARCHAR(25),
+    `City` VARCHAR(10) NOT NULL,
+    `School` VARCHAR(30) NOT NULL,
+    `HomePhone` VARCHAR(15),
+    `JobPhone` VARCHAR(15),
+    `MobilePhone1` VARCHAR(15),
+    `MobilePhone2` VARCHAR(15)
+)  ENGINE=INNODB;
+
+LOAD DATA INFILE '/var/lib/mysql-files/denormalized-data.csv'
+INTO TABLE UNF
+CHARACTER SET latin1
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
 DROP TABLE IF EXISTS Student;
 
 CREATE TABLE Student (
-	StudentId int,
-	Name varchar(255)
-)
-ENGINE=INNODB
-;
+    StudentId INT NOT NULL,
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    CONSTRAINT PRIMARY KEY (StudentId)
+)  ENGINE=INNODB;
+
+INSERT INTO Student (StudentID, FirstName, LastName) 
+SELECT DISTINCT Id, SUBSTRING_INDEX(Name, ' ', 1), SUBSTRING_INDEX(Name, ' ', -1) 
+FROM UNF;se iths;
 
