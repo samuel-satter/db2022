1d2e3879c4b82511f2e2c0e91e6978ac8056dbda
samuel-satter <97049361+samuel-satter@users.noreply.github.com>
2023-01-14 20:35:14 +0100

    Started adding java

---
+++ app/src/main/java/se/iths/Student.java
@@ -0,0 +1,34 @@
+package se.iths;
+
+public class Student {
+
+    private final int id;
+
+    String firstName;
+
+    String lastName;
+
+
+    public Student(int StudentId, String firstName, String lastName){
+        this.id = StudentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+}
